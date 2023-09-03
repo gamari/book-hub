85a0c744fdf7be49643932c5483a28e9942e62bc
gamari <gamari4552@gmail.com>
2023-09-03 23:23:24 +0900

    検索処理

--- src/main/java/gamari/app/features/books/apis/BookApiController.java
+++ src/main/java/gamari/app/features/books/apis/BookApiController.java
@@ -1,26 +1,31 @@
 package gamari.app.features.books.apis;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import gamari.app.features.books.mappers.BookMapper;
-import gamari.app.features.books.models.Book;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookApiController {
     @Autowired
-    private BookMapper bookMapper;
+    private RestTemplate restTemplate;
 
     @GetMapping("/search")
-    public List<Book> searchBooks(@RequestParam String keyword) {
-        System.out.println("AAAAAAAAAA");
-        System.out.println("keyword: " + keyword);
-        return bookMapper.findByTitleContaining(keyword);
+    public ResponseEntity<Map> searchBooks(@RequestParam String keyword) {
+        try {
+            String googleBooksAPIUrl = "https://www.googleapis.com/books/v1/volumes?q=" + keyword;
+            ResponseEntity<Map> response = restTemplate.getForEntity(googleBooksAPIUrl, Map.class);
+            return response;
+        } catch (Exception e) {
+            System.out.println(e);
+            throw e;
+        }
     }
 }
--- src/main/resources/static/js/searchBooks.js
+++ src/main/resources/static/js/searchBooks.js
@@ -4,23 +4,29 @@ function searchBooks() {
     fetch(`/api/books/search?keyword=${query}`)
         .then((response) => response.json())
         .then((data) => {
+            console.log(data);
+
+
             const searchResults = document.getElementById("searchResults");
             searchResults.innerHTML = "";
 
-            if (data.length === 0) {
+            if (!data?.items?.length) {
                 // 検索結果が空の場合
                 const noResultsDiv = document.createElement("div");
                 noResultsDiv.innerText = "見つかりませんでした";
                 searchResults.appendChild(noResultsDiv);
             } else {
                 // 検索結果がある場合
-                data.forEach((book) => {
+                data.items.forEach((book) => {
+                    const { title, industryIdentifiers } = book.volumeInfo;
+                    const isbn = industryIdentifiers[1].identifier;
+
                     const bookDiv = document.createElement("div");
-                    bookDiv.innerText = book.title;
+                    bookDiv.innerText = title + " " + isbn;
 
                     const addButton = document.createElement("button");
                     addButton.innerText = "登録";
-                    addButton.onclick = () => addReadingBook(book.id);
+                    addButton.onclick = () => addReadingBook(isbn);
 
                     bookDiv.appendChild(addButton);
                     searchResults.appendChild(bookDiv);
